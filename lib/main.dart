9b055b5b8477377c5994ed5a8d0a2337b5d3014b
nprgin <nprgin@tvz.hr>
2024-04-16 16:46:54 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MainApp());
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello There"),
+        ),
+        body: const Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
+
